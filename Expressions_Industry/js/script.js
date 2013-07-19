7d5f927ef26c3d94b90e80919f98930494bf5c45
Monica Perez <MonicaPerez@Fullsail.edu>
2013-07-18 20:09:59 -0700

    Expressions_Industry

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -4,4 +4,6 @@ var hoursWorkedWeekly = prompt("Hello. Please enter the amount of hours you work
 var daysWeekWorked = prompt("Thank you. Now, please enter how many days a week you work.");// setting daysWeekWorked to ask the user a question that will be held in the variable.
 var hoursWorkedDaily = hoursWorkedWeekly / daysWeekWorked; //Setting hoursWorkedDaily to be equal to hoursWorkedWeek divided by daysWeekWorked
 var hoursPerDay = 24; // the variable is named hoursPerDay and it is holding the number value of 24.
-var hoursFreeDaily = hoursPerDay - hoursWorkedDaily; // the variable is named hoursFreeDaily and is equal to the variable hoursPerDay
+var hoursFreeDaily = hoursPerDay - hoursWorkedDaily; // the variable is named hoursFreeDaily and is equal to the variable hoursPerDay subtracted by the variable hoursWorkedDaily
+var daysPerWeek = 7; // the variable named daysPerWeek is equal to the value of 7
+var hoursFree = hoursFreeDaily * daysPerWeek;
