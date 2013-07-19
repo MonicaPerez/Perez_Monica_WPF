3eb41a89d1eed129defddb602fd952588af65d53
Monica Perez <MonicaPerez@Fullsail.edu>
2013-07-18 19:26:27 -0700

    Expressions_Classwork

--- Expressions Classwork/js/script.js
+++ Expressions Classwork/js/script.js
@@ -1,6 +1,6 @@
 //Monica Perez - July 18, 2013 - Expressions Classwork
 
-/*var yearBorn = 1955;
+var yearBorn = 1955;
 
 
 console.log("Hello World");
@@ -27,7 +27,7 @@ var phrase = "I don't know";
 
 //Arrays
 
-var muppetName = ["Kermit" . "Miss Piggy"]; //0. 1. 2. 3
+/*var muppetName = ["Kermit" . "Miss Piggy"]; //0. 1. 2. 3
 
 muppetNames[1] = "Rizzo"; // change index #1 to Rizzo
 muppetNames[2] = "Fozzy";
@@ -63,5 +63,46 @@ var orangeBins = [234, 567, 883];
 
 var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
 console.log(total);
+
+//Expressions - Concatenating Strings
+
+var fistNames = "Kermit";
+var lastName = "The Frog";
+var fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+var a = "6";
+var b = "7";
+
+var result = a + b;
+console.log(result);
+
+
+//Expressions - Assignment Operators
+
+var a = 3;
+a += 1; // a = a + 4
+//a ++ //a = a + 1 // a += 1
+console.log(a);
+
+
+//Prompt and Alert
+
+var userInput = prompt("Enter your year of birth:");
+console.log(userInput);
+
+var width = prompt("We are calculating the area of a rectangle. Please enter the width.");
+var height = prompt("Please enter the height.");
+var area = width * height; // calculates with that info
+//console.log(area); // prints it out to the console
+var result = "The area of your rectangle is " + area + " sq feet";
+alert(result);
+
+
+
+
+
+
 
 
