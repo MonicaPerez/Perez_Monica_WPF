4cdf37980ada65545da24d33a6429d218994c266
Monica Perez <MonicaPerez@Fullsail.edu>
2013-07-18 20:29:14 -0700

    Expressions_Wacky

---
+++ Expressions_Wacky/js/script.js
@@ -0,0 +1,3 @@
+//Monica Perez - Assignment: Expressions , Wacky - July 18, 2013
+//How many calories women can eat a day without gaining weight.
+//he calculation for a woman's BMR = 655 + (4.35 * weight in pounds) + (4.7 * height in inches) - (4.7 * age in years)
