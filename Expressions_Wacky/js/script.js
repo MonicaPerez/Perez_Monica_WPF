327693e6b27ad24bae8e21f1402f9872cb1910e8
Monica Perez <MonicaPerez@Fullsail.edu>
2013-07-18 20:29:59 -0700

    Expressions_Wacky

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,3 +1,4 @@
 //Monica Perez - Assignment: Expressions , Wacky - July 18, 2013
 //How many calories women can eat a day without gaining weight.
-//he calculation for a woman's BMR = 655 + (4.35 * weight in pounds) + (4.7 * height in inches) - (4.7 * age in years)
+//The calculation for a woman's BMR = 655 + (4.35 * weight in pounds) + (4.7 * height in inches) - (4.7 * age in years)
+alert("If you are a woman, we are going to find out your Basal Metabloic Rate (MBR).");// i am alerting the internet user letting them know what the prupose of this page is.
