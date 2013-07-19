39aba2cd6a930a02244484d6fce5adc5046bcc0d
Monica Perez <MonicaPerez@Fullsail.edu>
2013-07-18 19:59:06 -0700

    Expressions_Industry

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,5 +1,5 @@
 //Monica Perez - Expressions Assignment: Industry - July 18, 2013
 //I am trying to find out how many hours per week a person has of free time.
-var hoursWorkedWeekly = prompt("Hello. Please enter the amount of hours you work weekly."); //
+var hoursWorkedWeekly = prompt("Hello. Please enter the amount of hours you work weekly."); //setting hoursWorkedWeekly to prompt the user and ask them to enter the amount of hours they work a week.
 var daysWeekWorked = prompt("Thank you. Now, please enter how many days a week you work.");// setting daysWeekWorked to ask the user a question that will be held in the variable.
 var hoursWorkedDaily = hoursWorkedWeekly / daysWeekWorked; //Setting hoursWorkedDaily to be equal to hoursWorkedWeek divided by daysWeekWorked
