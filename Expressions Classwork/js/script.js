41da85f00ab7173f5e8dc4516d0d9d2faed92501
Monica Perez <MonicaPerez@Fullsail.edu>
2013-07-18 18:25:34 -0700

    Expressions_Classwork

---
+++ Expressions Classwork/js/script.js
@@ -0,0 +1,18 @@
+//Monica Perez - July 18, 2013 - Expressions Classwork
+
+var yearBorn = 1955;
+
+/*
+console.log("Hello World");
+console.log(yearBorn);
+*/
+
+alert(yearBorn);
+
+//Defining Variables
+age = 38
+var age;
+//Storage containers = value
+
+var yearOfBirth = 1907; //declaring and defining on the same line
+
