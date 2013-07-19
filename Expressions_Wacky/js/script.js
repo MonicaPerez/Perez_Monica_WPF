7ee8095fa80ee19cc668fb6b7cc4b2583c9035f1
Monica Perez <MonicaPerez@Fullsail.edu>
2013-07-18 20:33:15 -0700

    Expressions_Wacky

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,6 +1,7 @@
 //Monica Perez - Assignment: Expressions , Wacky - July 18, 2013
 //How many calories women can eat a day without gaining weight.
 //The calculation for a woman's BMR = 655 + (4.35 * weight in pounds) + (4.7 * height in inches) - (4.7 * age in years)
+
 alert("If you are a woman, we are going to find out your Basal Metabloic Rate (MBR).");// i am alerting the internet user letting them know what the prupose of this page is.
 var age = prompt ("Please enter your age."); // setting the variable age to prompt the user to enter there age and saving it into the console for later use
 var height = prompt ("Please enter your height in inches."); // setting the variable height to prompt the user to enter there height in inches and saving it into the console for later use
