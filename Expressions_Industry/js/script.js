16ee04d639dd048f3489c52ff7caf8258b646a0d
Monica Perez <MonicaPerez@Fullsail.edu>
2013-07-18 19:40:45 -0700

    Expressions_Industry

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,2 +1,4 @@
 //Monica Perez - Expressions Assignment: Industry - July 18, 2013
-
+//I am trying to find out how many hours per week a person has of free time.
+var hoursWorkedWeekly = prompt("Please enter the amount of hours you work weekly.");
+var daysWeekWorked = prompt
