973526b81c93842a3f41ac6a1dfea87bdd365075
Monica Perez <MonicaPerez@Fullsail.edu>
2013-07-25 15:09:38 -0700

    Conditionals_Personal

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,1 +1,5 @@
 //Monica Perez - July 25, 2013 - Personal Conditional
+
+//If a person intakes less than the average calories burned and you burn over 500 calories while exercising than they can treat themselves to icecream.
+//An average person burns 1300 calories daily, without exercising
+//The aver calories of a small icecream is 230 calories.
