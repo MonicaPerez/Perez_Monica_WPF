49f6c221a844e4f50baf7dcda32e587234cf61e8
Monica Perez <MonicaPerez@Fullsail.edu>
2013-07-25 14:54:16 -0700

    Conditionals_Industry

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,2 +1,3 @@
 //Monica Perez - July 25, 2013 - Industry Conditional
 //I am required to work 45 hours a week. If I complete them before Friday, then I will have the weekend off. Else I don't complete them by Friday, then I have to work the weekend
+var hoursWorked = prompt("Please enter: How many hours have you worked during the week.") // naming the variable hoursWorked and setting it to equal a prompt to acquire information from the user
