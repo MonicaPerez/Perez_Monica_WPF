3530bd4ef5c9fd4c0b8dd32590a06a242f24e9db
Monica Perez <MonicaPerez@Fullsail.edu>
2013-07-25 12:34:13 -0700

    Conditionals_Classwork

--- Conditionals_Classwork/js/script.js
+++ Conditionals_Classwork/js/script.js
@@ -1,1 +1,20 @@
 //Monica Perez - July 25, 2013 - Conditionals Worksheet
+
+//If or Else Condtion
+
+//sunny = true;
+if (sunny){
+	goToTheBeach();
+
+	if(warmWater){
+		wearNewSuit();
+}
+}else{ 
+	goToTheMovies();
+}
+
+
+temp = 82
+if(temp>70){
+	goToTheBeach
+}
