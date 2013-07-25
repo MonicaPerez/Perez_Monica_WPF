062be5b0081cf2204eb8a52f56b74c04a54ba563
Monica Perez <MonicaPerez@Fullsail.edu>
2013-07-25 14:52:08 -0700

    Conditionals_Industry

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,1 +1,2 @@
 //Monica Perez - July 25, 2013 - Industry Conditional
+//I am required to work 45 hours a week. If I complete them before Friday, then I will have the weekend off. Else I don't complete them by Friday, then I have to work the weekend
