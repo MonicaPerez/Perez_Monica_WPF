b3fc23bf3bed019de6c6ec261827fb627514e79b
Monica Perez <MonicaPerez@Fullsail.edu>
2013-07-25 15:16:44 -0700

    Conditionals_Personal

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -6,4 +6,7 @@
 
 var todayCalorieIntake = prompt("Please enter the number of calories have you eaten today.");//naming the variable todayCalorieIntake assigning it to prompt the user and hold the aquired information.
 var averageCaloriesBurnedDaily = 1300; //naming the variable averageCaloriesBurnedDaily and assigning it to he value 1,300.
-var exercise = prompt("Please enter ")
+var exercise = prompt("Please enter the number of calories you burned while exercising."); //setting the variable to be named exercise and assigning that variable to prompt the user and hold the aquired information.
+if(todayCalorieIntake < averageCaloriesBurnedDaily && exercise >= 250){
+	
+}
