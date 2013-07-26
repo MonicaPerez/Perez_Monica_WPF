e11ad7af3aca1449dba55a9f0a02bc159092c580
Monica Perez <MonicaPerez@Fullsail.edu>
2013-07-25 18:05:53 -0700

    Conditionals_Personal

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -4,9 +4,10 @@
 //An average person burns 1300 calories daily, without exercising
 //The aver calories of a small icecream is 230 calories.
 
+alert("Can you have some icecream?")
 var todayCalorieIntake = prompt("Please enter the number of calories have you eaten today.");//naming the variable todayCalorieIntake assigning it to prompt the user and hold the aquired information.
-var averageCaloriesBurnedDaily = 1300; //naming the variable averageCaloriesBurnedDaily and assigning it to he value 1,300.
+var averageCaloriesBurnedDaily = 1300; //naming the variable averageCaloriesBurnedDaily and assigning it to the value 1,300.
 var exercise = prompt("Please enter the number of calories you burned while exercising."); //setting the variable to be named exercise and assigning that variable to prompt the user and hold the aquired information.
-if(todayCalorieIntake < averageCaloriesBurnedDaily && exercise >= 250){
-	
+if(todayCalorieIntake < averageCaloriesBurnedDaily && exercise >= 250){//setting an if elese statement and the if part is saying if todayCalorieIntake is less than averageCaloriesBurned and they burned more than 250 calories from exercising then the following statement will print.
+	console.log("Yay! You may eat some icecream.");
 }
