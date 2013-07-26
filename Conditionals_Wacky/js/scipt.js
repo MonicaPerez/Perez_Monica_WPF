b04687babda0d9268b023a2c505c087d673e7755
Monica Perez <MonicaPerez@Fullsail.edu>
2013-07-25 18:45:22 -0700

    Conditionals_Wacky

--- Conditionals_Wacky/js/scipt.js
+++ Conditionals_Wacky/js/scipt.js
@@ -7,7 +7,8 @@ var licensedDriversAge = prompt("Please enter the age of the licensed driver tha
 if (age >= 17){//setting up the statement saying if the variable age is greater than 17 they can drive with anyone.
 	console.log("You are old enough to drive with a licensed driver of any age.");//printing to the console the statement that applies to the if statement in the previous line.
 }else if (age < 16){//stating that if the first rule does not apply then this rule might apply. if the variable age is less than 16 then the following statement will print to the console.
-	console.log("Sorry you are not old enough to drive.")//printing to the console the statement if the previous line is true
+	console.log("Sorry you are not old enough to drive.";//printing to the console the statement if the previous line is true
 }else if (age = 16 && otherDriverAge >= 21){//if the previous two statements do not apply then it moves onto this statement which states if the variable age is equal to 16 and the variable licensedDriversAge is greater than or equal to 21 then the following statement will print to the console.
+	console.log("You can drive only if the licensed driver has had their license for 3 consecutive years.");
 
 }
