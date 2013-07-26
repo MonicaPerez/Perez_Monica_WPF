11d0ea2e7f082dd559efa9cc967592f5e8b0424d
Monica Perez <MonicaPerez@Fullsail.edu>
2013-07-25 19:01:25 -0700

    Conditionals_Wacky

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -8,7 +8,7 @@ if (age >= 17){//setting up the rule for the else if statement saying if the var
 	console.log("You are old enough to drive with a licensed driver of any age.");//printing to the console the statement that applies to the if statement in the previous line.
 }else if (age < 16){//stating that if the first rule does not apply then this rule might apply. if the variable age is less than 16 then the following statement will print to the console.
 	console.log("Sorry you are not old enough to drive.");//printing to the console the statement if the previous line is true
-}else if (age = 16 && otherDriversAge >= 21){//if the previous two statements do not apply then it moves onto this statement which states if the variable age is equal to 16 and the variable licensedDriversAge is greater than or equal to 21 then the following statement will print to the console.
+}else if (age = 16 && licensedDriversAge >= 21){//if the previous two statements do not apply then it moves onto this statement which states if the variable age is equal to 16 and the variable licensedDriversAge is greater than or equal to 21 then the following statement will print to the console.
 	console.log("You can drive only if the licensed driver has had their license for 3 consecutive years.");
 }else{//stating if none of the pervious statements apply then the following is to print to the console.
 	console.log("You are old enough to drive by yourself");// printing to the console the statement if none of the previous rules applied to the user's input
