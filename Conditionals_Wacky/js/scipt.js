0a222e0bc5b8209eda0d660210e5733c27c30296
Monica Perez <MonicaPerez@Fullsail.edu>
2013-07-25 18:27:58 -0700

    Conditionals_Wacky

--- Conditionals_Wacky/js/scipt.js
+++ Conditionals_Wacky/js/scipt.js
@@ -1,2 +1,5 @@
 //Monica Perez - July 25, 2013 - Wacky Conditional
+//In New Jersey you can recieve your drivers permit at 16 years old. You must drive with a licensed driver who is 21 or older, and has had a license for 3 consecutive years.
 
+var age = prompt("What is your age?");//naming the variable age and assigning a prompt to that variable; asking the user what their age is.
+var otherDriverAge = prompt("How old is the person that is driving with you?");
