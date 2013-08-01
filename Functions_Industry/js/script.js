ea809da7e637764e87386c454733d3e1396b11fc
Monica Perez <MonicaPerez@Fullsail.edu>
2013-08-01 14:03:07 -0700

    Functions_Industry

---
+++ Functions_Industry/js/script.js
@@ -0,0 +1,2 @@
+//Monica Perez - Industry Function - August 1, 2013
+//If I own a web designing company and I have 28 projects to work on each consisting of 3 hours and I have 5 employees. How long will this take?
