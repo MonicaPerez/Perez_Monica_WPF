e07e3ace5acfd3c8fe539f1ccb4201c31ccc4e8a
Monica Perez <MonicaPerez@Fullsail.edu>
2013-08-01 18:20:00 -0700

    Functions_Industry

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -8,7 +8,9 @@ calcHours (employees, projects);
 
 function calcHours(projects, employees){ //setting up a function to store the variable named project and the variable named employees
 	var total = projects / employees ;//naming the variable total and making it equal to the variable projects divided by the variable employees
-		if(employees > 4 && projects <= 20){//setting an if statement to say if there are 
-
-		} 
-}
+		if(employees > 4 && projects <= 20){//setting an if statement to say if there are more employees than 4 and less than or equal to 20 projects then the following statement will print to console.
+			console.log("It will require " + total + " hours per person to finish all of the projects.")// printing to the console the statement if the previous requirements were met
+		}else{//setting the else part of the if statement, if the user input does not meet the requirements of the if statement, then the following statement will print.
+			console.log("You should hire more workers.");// printing to the console the statement that will be printed if the if statement is not met.
+		}//closing out the else statement
+}//closing out the function
