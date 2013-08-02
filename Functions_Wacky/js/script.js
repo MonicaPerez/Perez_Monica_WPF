a7246e222777044702014ba3b17c40d89fe96c88
Monica Perez <MonicaPerez@Fullsail.edu>
2013-08-01 19:08:15 -0700

    Functions_Wacky

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,4 +1,4 @@
 //Monica Perez - Wacky Function - August 1, 2013
 // If the user is a male then this will calculate their BMR 
 
-var male = 
+var male = prompt("If you are a male please enter the number '1'. If you are a female please enter the numer '2'.")
