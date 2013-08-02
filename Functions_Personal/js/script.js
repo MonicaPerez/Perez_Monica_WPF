2b01682e022a3be8a8ccf3d10e761dbfe9424b27
Monica Perez <MonicaPerez@Fullsail.edu>
2013-08-01 18:37:38 -0700

    Functions_Personal

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -6,3 +6,4 @@ var calcPetFood = function(petFood, days){//naming the variable calcPetFood and 
 
 }
 var petFood = prompt("Please enter the number of cans of pet food you use in 1 day.");
+var days = prompt("Please enter the amount of days in this month.");
