6be951faa3adbae70b5d04ebababc90a63f9d7b5
Monica Perez <MonicaPerez@Fullsail.edu>
2013-08-01 18:48:47 -0700

    Functions_Personal

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -3,11 +3,12 @@
 // How much cans of pet food do they go through in one month?; If they are feeding their animals more than one can a day then they need to go out and buy another 24 pack of pet food.
 
 var calcPetFood = function(petFood, days){//naming the variable calcPetFood and setting it to a function, setting that function to store the user input for the variable named petFood and the user input for the variable named days
-
+	var total = petFood * days 
+	return total;
 }
 var petFood = prompt("Please enter the number of cans of pet food you use in 1 day.");
 var days = prompt("Please enter the amount of days in this month.");
 var print = calcPetFood (petFood, days);
 
-answer = (petFood > 1) ? "You need to purchase another 30 pack of pet food for your lovely animal." : "You will use " + print + " cans of petFood this month."
+answer = (print > 1) ? "You need to purchase another 30 pack of pet food for your lovely animal." : "You will use " + print + " cans of petFood this month."
 console.log(answer);
