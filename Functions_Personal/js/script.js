399e9dc8e291fe36c7a561de2e5a2415aa8b8397
Monica Perez <MonicaPerez@Fullsail.edu>
2013-08-01 18:37:02 -0700

    Functions_Personal

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,1 +1,8 @@
 //Monica Perez - Personal Function - August 1, 2013
+// For every person who buys a 24 pack of wet canned pet food
+// How much cans of pet food do they go through in one month?; If they are feeding their animals more than one can a day then they need to go out and buy another 24 pack of pet food.
+
+var calcPetFood = function(petFood, days){//naming the variable calcPetFood and setting it to a function, setting that function to store the user input for the variable named petFood and the user input for the variable named days
+
+}
+var petFood = prompt("Please enter the number of cans of pet food you use in 1 day.");
