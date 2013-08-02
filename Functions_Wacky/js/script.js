0e0a12a0adb705f7022c2ba7c49952a3097a5c99
Monica Perez <MonicaPerez@Fullsail.edu>
2013-08-01 19:21:30 -0700

    Functions_Wacky

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -10,14 +10,15 @@ if (male = 1){
 	calcMaleBmr(weight, height, age);
 	function calcMaleBmr(w,h,a){
 		var maleBmr = 66 + (6.23 * w) + (12.7 * h) - (6.8 * a);
-		console.log("Your Basil Metabolic Rate (BMR) is " + bmr + ".");
+		console.log("Your Basil Metabolic Rate (BMR) is " + maleBmr + ".");
 	}
 }else if (male = 2){
 	calcFemaleBmr(weight, height, age);
 	function calcFemaleBmr(w,h,a){
-		
+		var femaleBmr = 655 + (4.35 * w) + (4.7 * h) - (4.7 * a);
+		console.log("Your Basil Metabolic Rate (BMR) is " + femaleBmr + ".");
 	}
-
 }else{
+	console.log("I did not understand your input.")
 
 }
