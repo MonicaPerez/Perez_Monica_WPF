6c3dc622f2189fee6e51dde76a38895e395fbc6c
Monica Perez <MonicaPerez@Fullsail.edu>
2013-08-01 18:08:46 -0700

    Functions_Industry

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -4,7 +4,11 @@
 var employees = prompt("Please enter how many employees are schedueled today?"); //naming the variable employees and setting it to prompt the user  for information.
 var projects = prompt("Please enter the amount of projects you have to work on.");// naming the variable project and setting it to prompt the user for information.
 
-calcHours (employees, projects)
-function calcHours(projects, employees){
+calcHours (employees, projects);
 
+function calcHours(projects, employees){ //setting up a function to store the variable named project and the variable named employees
+	var total = projects / employees ;//naming the variable total and making it equal to the variable projects divided by the variable employees
+		if(employees > 4 && projects <= 20){//setting an if statement to say if there are 
+
+		} 
 }
