3a532757c8c054838c1df13ad6e939a77f6bc448
Monica Perez <MonicaPerez@Fullsail.edu>
2013-08-01 17:39:24 -0700

    Functions_Industry

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,2 +1,4 @@
 //Monica Perez - Industry Function - August 1, 2013
 //If I own a web designing company and I have 28 projects to work on each consisting of 3 hours and I have 5 employees. How long will this take?
+
+var employees = prompt("How many employees are schedueled today?"); //naming the variable employees and setting it to prompt the user  for information
