6d9af6966f59b4b021b810ad67e6c255c37d994c
Monica Perez <MonicaPerez@Fullsail.edu>
2013-08-01 19:15:13 -0700

    Functions_Wacky

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,7 +1,7 @@
 //Monica Perez - Wacky Function - August 1, 2013
 // If the user is a male then this will calculate their BMR 
 
-var male = prompt("If you are a male please enter the number '1'. If you are a female please enter the numer '2'.");
+var male = prompt("If you are a male please enter the number 1. If you are a female please enter the numer 2.");
 var age = prompt("Please enter your age in years.");
 var height = prompt("Please enter your height in inches.");
 var weight = prompt("Please enter your weight in pounds.");
@@ -12,4 +12,8 @@ if (male = 1){
 		var maleBmr = 66 + (6.23 * w) + (12.7 * h) - (6.8 * a);
 		console.log("Your Basil Metabolic Rate (BMR) is " + bmr + ".");
 	}
+}else if (male = 2){
+
+}else{
+
 }
