1332e0eea1f4ecc05194e4d68252ce02dd0b24e0
Monica Perez <MonicaPerez@Fullsail.edu>
2013-08-01 19:37:15 -0700

    Functions_Wacky

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -6,13 +6,13 @@ var age = prompt("Please enter your age in years.");
 var height = prompt("Please enter your height in inches.");
 var weight = prompt("Please enter your weight in pounds.");
 
-if (male = 1){
+if(male <= 1){
 	calcMaleBmr(weight, height, age);
 	function calcMaleBmr(w,h,a){
 		var maleBmr = 66 + (6.23 * w) + (12.7 * h) - (6.8 * a);
 		console.log("Your Basil Metabolic Rate (BMR) is " + maleBmr + ".");
 	}
-}else if (male = 5){
+}else if(male >= 5){
 	calcFemaleBmr(weight, height, age);
 	function calcFemaleBmr(w,h,a){
 		var femaleBmr = 655 + (4.35 * w) + (4.7 * h) - (4.7 * a);
@@ -20,6 +20,4 @@ if (male = 1){
 	}
 }else{
 	console.log("I did not understand your input.")
-
 }
-/* 655 + 595.95 + 314.9 - 103.4
